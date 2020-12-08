b07419870f5cd86cfd79b992251915128db31b68
Danek <janek@projmen.pl>
2020-12-08 21:06:51 +0100

    Update

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
+import time
+import re
 
 import markovify
 from wykop import WykopAPI
-import time
 
 key = os.environ.get('WYKOP_TAG_KEY')
 secret = os.environ.get('WYKOP_TAG_SECRET')
@@ -11,14 +12,11 @@ api = WykopAPI(key, secret, output='clear')
 
 
 def remove_tags(text):
-    result = []
-    for line in text.split('\n'):
-        result.append(' '.join([word for word in line.split(' ') if '#' not in word]))
-    return '\n'.join([x for x in result if x])
+    return re.sub(r'#(\w*[0-9a-zA-Z]+\w*[0-9a-zA-Z])', '', text)
 
 
-def format(text: str):
-    return text.replace('\n\n', '\n')
+def remove_empty_lines(text: str):
+    return '\n'.join([line for line in text.split("\n") if line.strip()])
 
 
 def filter_to_short(text, min_words=5):
@@ -32,7 +30,7 @@ def get_messages(tag, pages=100):
         x = api.tag(tag, page=i)
         m = [entry.entry.body for entry in x if entry.type == 'entry']
         messages += m
-        time.sleep(1)
+        time.sleep(0.3)
     return '\n'.join(messages)
 
 
@@ -46,14 +44,17 @@ def load(tag):
 
 def main():
     tag = "polityka"
-    messages = get_messages(tag, 100)
+    messages = get_messages(tag, 500)
     messages = remove_tags(messages)
-    messages = format(messages)
+    messages = remove_empty_lines(messages)
     text = filter_to_short(messages)
 
     save(text, tag)
 
-    model = markovify.Text(text, state_size=3)
+    model = markovify.Text(load(tag), state_size=3)
 
     for _ in range(5):
         print(model.make_sentence(tries=100))
+
+
+main()
